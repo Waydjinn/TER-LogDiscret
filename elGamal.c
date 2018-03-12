aec49289148df4086cdb95763812d327cf1cbc0e
Martin Waydjinn Zaarour <waydjinn@DESKTOP-BV724LT.localdomain>
2018-03-12 19:50:14 +0100

    moyen

--- elGamal.c
+++ elGamal.c
@@ -17,23 +17,45 @@ typedef struct
   mpz_t x;	    /* exposant secret */
 } sk;
 
-void key_gen()
+void key_gen(pk *pk;)
 {
+  gmp_printf ("%s is an mpz\n", "here");
+
+  //pk *pk;
+
+  gmp_printf ("%s is an mpz\n", "here");
+
+  mpz_set_ui(pk pk.p, 6883);
+  mpz_set_ui(pk->g, 4344);
+
+  gmp_printf ("%s is an mpz\n", "here");
+
   gmp_randstate_t state;
   mpz_t n;
   mpz_init(n);
   mpz_t res;
   mpz_init(res);
-  mpz_set_ui(n, 10);
+  mpz_set(n, pk->p);
   gmp_randinit_default (state);
   mpz_urandomm (res, state, n);
 
+
+  mpz_powm_sec(pk->y, pk->g, res, pk->p);
+  gmp_printf ("%s is an mpz %Zd\n", "here", pk->y);
+
+
   mpz_clear(n);
+  mpz_clear(pk->g);
+  mpz_clear(res);
+  mpz_clear(pk->p);
+  mpz_clear(pk->y);
 
 
 }
 
 int main()
 {
+  key_gen();
+
   return 0;
 }
