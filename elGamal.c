45f7593eff8d678253b266681b64c5995f5a4c46
Martin Waydjinn Zaarour <waydjinn@DESKTOP-BV724LT.localdomain>
2018-03-14 23:57:55 +0100

    JC code sale dechet

--- elGamal.c
+++ elGamal.c
@@ -6,7 +6,7 @@ typedef struct
 {
   mpz_t p;	    /* nb premier */
   mpz_t g;	    /* generateur du groupe */
-  mpz_t y;	    /* g^x mod p */
+  mpz_t h;	    /* g^x mod p */
 } pk;
 
 
@@ -14,7 +14,7 @@ typedef struct
 {
   mpz_t p;	    /* nb premier */
   mpz_t g;	    /* generateur du groupe */
-  mpz_t y;	    /* g^x mod p */
+  mpz_t h;	    /* g^x mod p */
   mpz_t x;	    /* exposant secret */
 } sk;
 
@@ -24,14 +24,12 @@ typedef struct
   mpz_t c2;
 } C;
 
-void key_gen(pk *pk, sk *sk, mpz_t p, mpz_t g, mpz_t y, mpz_t x)
+void key_gen(pk *pk, sk *sk, mpz_t p, mpz_t g, mpz_t h, mpz_t x)
 {
 /////////initialisation////////
 
-
   mpz_set_ui(pk->p, 8999);
   mpz_set_ui(pk->g, 6426);
-
 
   gmp_printf ("%s %Zd\n", "p =", pk->p);
   gmp_printf ("%s %Zd\n", "g =", pk->g);
@@ -42,44 +40,51 @@ void key_gen(pk *pk, sk *sk, mpz_t p, mpz_t g, mpz_t y, mpz_t x)
   gmp_randinit_default (state);
   gmp_randseed_ui(state, seed);
   mpz_urandomm (sk->x, state, pk->p);
-  mpz_powm_sec(pk->y, pk->g, sk->x, pk->p);
+  mpz_powm_sec(pk->h, pk->g, sk->x, pk->p);
 
   gmp_printf ("%s %Zd\n", "x random", sk->x);
-  gmp_printf ("%s %Zd\n", "g^x =", pk->y);
-
-
-
+  gmp_printf ("%s %Zd\n", "g^x =", pk->h);
 
   gmp_randclear(state);
 
-
 }
 
-void encryption(pk *pk,sk *sk, C *C ,mpz_t p, mpz_t g, mpz_t y, mpz_t x, mpz_t m, mpz_t c1, mpz_t c2)
+void encryption(pk *pk,sk *sk, C *C ,mpz_t p, mpz_t g, mpz_t h, mpz_t x, mpz_t m, mpz_t c1, mpz_t c2)
 {
-  gmp_printf ("%s %Zd\n", "x random=", sk->x);
-  gmp_printf ("%s %Zd\n", "g^x =", pk->y);
 
-  mpz_t n;
-  mpz_init(n);
-//  mpz_t s;
-//  mpz_init(s);
+
+  mpz_t y;
+  mpz_init(y);
+  mpz_t s;
+  mpz_init(s);
+  mpz_t tmp;
+  mpz_init(tmp);
   unsigned long int seed = time(NULL);
   gmp_randstate_t state;
   gmp_randinit_default (state);
   gmp_randseed_ui(state, seed);
-  mpz_urandomm (n, state, pk->p);
-  mpz_powm_sec(C->c1, pk->y, n, pk->p);
-  gmp_printf ("%s %Zd\n", "g^xy", C->c1);
+  mpz_urandomm (y, state, pk->p);
 
+  mpz_mul(tmp, sk->x, y); //tmp prend la valeur x*y
+  mpz_powm_sec(C->c1, pk->g, y, pk->p); //c1 = g^y
+  mpz_powm_sec(s, pk->g, tmp, pk->p); 
 
-  mpz_mul(C->c2, m, C->c1);
+  gmp_printf ("%s %Zd\n", "g^xy", s);
+  gmp_printf ("%s %Zd\n", "g^xy", tmp);
 
+  gmp_printf ("%s %Zd\n", "g^xy", y);
+
+  mpz_mul(C->c2, m, s);
+  gmp_printf ("%s %Zd\n", "c1", C->c1);
   gmp_printf ("%s %Zd\n", "c2", C->c2);
 
+  mpz_clear(y);
 
+}
 
-
+void decryption()
+{
+  mpz_t s;
 }
 
 int main()
@@ -87,27 +92,25 @@ int main()
   pk pk;
   sk sk;
   C C;
-  mpz_t p,g,x,y,m,c1,c2;
+  mpz_t p,g,x,h,m,c1,c2;
   mpz_init (pk.p);
   mpz_init (pk.g);
   mpz_init(sk.x);
-  mpz_init(pk.y);
+  mpz_init(pk.h);
   mpz_init(m);
   mpz_set_ui(m, 204);
   mpz_init(C.c1);
   mpz_init(C.c2);
 
-
-  key_gen(&pk, &sk, p, g, y, x);
-  encryption(&pk, &sk,&C, p, g, y, x, m, c1, c2);
+  key_gen(&pk, &sk, p, g, h, x);
+  encryption(&pk, &sk,&C, p, g, h, x, m, c1, c2);
 
   gmp_printf ("%s %Zd\n", "c2 v2 ", C.c2);
-
 
   mpz_clear(pk.g);
   mpz_clear(sk.x);
   mpz_clear(pk.p);
-  mpz_clear(pk.y);
+  mpz_clear(pk.h);
   mpz_clear(m);
   mpz_clear(C.c1);
   mpz_clear(C.c2);
