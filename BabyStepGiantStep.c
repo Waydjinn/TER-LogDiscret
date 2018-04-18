be8a55c8d95901486a7b24ccaa561c2acfcabde7
AlexandreChrp <alexandre-09-03-93@hotmail.fr>
2018-04-18 11:59:32 +0200

    Ca marche !

--- BabyStepGiantStep.c
+++ BabyStepGiantStep.c
@@ -5,9 +5,11 @@
 
 
 
-void baby_step_giant_step(mpz_t resultat,mpz_t ordre,mpz_t generateur, mpz_t eltB){	
+int baby_step_giant_step(mpz_t resultat,mpz_t ordre,mpz_t generateur, mpz_t eltB){	
 	
 	int x;
+	
+	printf("Début du programme. \n");
 	
 	mpz_t resultatF;
 	mpz_init(resultatF);
@@ -31,11 +33,11 @@ void baby_step_giant_step(mpz_t resultat,mpz_t ordre,mpz_t generateur, mpz_t elt
 	}
 	
 	
-	//==============================================================================================
+	//======================================Baby Step========================================================
 	
 	
 	int j;
-
+	printf("Baby Step \n");
 	for(j=0;j<mpz_get_ui(entier);j++){ //Baby Step
 		
 		
@@ -55,42 +57,70 @@ void baby_step_giant_step(mpz_t resultat,mpz_t ordre,mpz_t generateur, mpz_t elt
 		mpz_clear(res);
 	
 	}
+	
+	printf("On affiche la table de hashage. \n");
+	for(x=0;x<mpz_get_ui(entier);x++){
+		gmp_printf("t[%d]=%Zd \n",x,t[x]);
+	}
+	
+	//====================================Calcul de g⁽⁻m⁾ : g_inv⁽m⁾ ===================================================
+	
+	mpz_t res1,g_inv,gamma; //Calculer alpha^(-m) mod n
+	mpz_init(res1);mpz_init(g_inv);mpz_init(gamma);
+	
 
 	
-	mpz_t res1,g_inv,mpz_entier,gamma; //Calculer alpha^(-m) mod n
-	mpz_init(res1);mpz_init(g_inv);mpz_init(mpz_entier),mpz_init(gamma);
+	int inv = mpz_invert(g_inv,generateur,ordre);//calcul de l'inverse de g
+	gmp_printf("g_inverse : generateur = %Zd  , ordre = %Zd \n",generateur,ordre);
 	
-
+	mpz_powm(res1,g_inv,entier,ordre); //g_inv à la puissance m
 	
-	int inv = mpz_invert(g_inv,generateur,ordre);
-	mpz_powm(res1,g_inv,entier,ordre);
+	gmp_printf("%Zd à la puissance %Zd = %Zd (modulo %Zd) \n",g_inv,entier,res1,ordre);
 	
 	mpz_clear(g_inv);
 	
+	//===============================Giant Step==========================================================
+	
 	mpz_set(gamma,eltB);
+
+
 
 	int i;
 	
-	
-	for(i=0;i<mpz_get_ui(entier)-1;i++){ //Giant Step
+	printf("Giant Step \n");
+	for(i=0;i<mpz_get_ui(entier);i++){ //Giant Step
+		printf("Pour i=%d \n",i);
+		//gmp_printf("t[%d]=%Zd compare gamma = %Zd \n",i,t[i],gamma);
 		
-		if(mpz_cmp(t[i],gamma)==0){
-			free(t);
-			mpz_mul_ui(resultatF,entier,i); // resF = entier * i
-			mpz_add_ui(resultatF,resultatF,i); // resF = (entier * i) +1
-			mpz_set(resultat,resultatF); //affectation 
-			mpz_clear(resultatF);
-			mpz_clear(entier);
-			mpz_clear(gamma);
-			mpz_clear(res1);
-			break;
+		for(j=0;j<mpz_get_ui(entier);j++){
 			
-		}else{
+			gmp_printf("Pour j=%d et t[j]=%Zd \n",j,t[j]);
 		
-			mpz_mul(gamma,gamma,res1);
-			mpz_mod(gamma,gamma,ordre);
+			if(mpz_cmp(t[j],gamma)==0){
+				printf("Solution trouvée ! \n");
 			
+				free(t);
+			
+				mpz_mul_ui(resultatF,entier,i); // resF = entier * i
+				mpz_add_ui(resultatF,resultatF,j); // resF = (entier * i) + j
+				mpz_set(resultat,resultatF); //affectation 
+			
+				mpz_clear(resultatF);
+				mpz_clear(entier);
+				mpz_clear(gamma);
+				mpz_clear(res1);
+				gmp_printf("Le résultat = %Zd \n",resultat);
+				return 0;
+			}	
 		}
+		
+			gmp_printf("gamma = %Zd * %Zd \n",gamma,res1);
+			mpz_mul(gamma,gamma,res1);
+			
+			mpz_mod(gamma,gamma,ordre);
+			gmp_printf("Je modifie gamma !\n",gamma);
+			
+		
 	}
 	
 		
@@ -100,17 +130,18 @@ void baby_step_giant_step(mpz_t resultat,mpz_t ordre,mpz_t generateur, mpz_t elt
 	
 int main (int argc, char *argv[]){
 	
-	mpz_t a,ordre,elt;
-	mpz_init(a);mpz_init(ordre);mpz_init(elt);
+	mpz_t generateur,ordre,elt;
+	mpz_init(generateur);mpz_init(ordre);mpz_init(elt);
 	mpz_t resultat;
 	mpz_init(resultat);
 	
-	mpz_set_ui(a,11);
-	mpz_set_ui(ordre,6);
-	mpz_set_ui(elt,10);
+	//
+	mpz_set_ui(generateur,2);//g=23
+	mpz_set_ui(ordre,1019);//n=53
+	mpz_set_ui(elt,5);//h=3    6¹⁰ = 60466176 mod 11 = 1
 	
-	baby_step_giant_step(resultat,a,ordre,elt);
-	gmp_printf("resultat=%Zd \n",resultat); //a=5
+	int g = baby_step_giant_step(resultat,ordre,generateur,elt);
+	//gmp_printf("resultat=%Zd \n",resultat); //a=5
 	
 	
 	
